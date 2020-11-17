5edabc2d28be7c263ea08b94a7e0dd1446a06ff4
Fred <email@Fred.com>
2020-11-17 17:00:58 -0500

    wowie

--- login.sh
+++ login.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $1 = "user" && $2 = "mdp" ]] 
 then
-	echo "vous êtes connectés"
+	echo "vous êtes connectés | you are connected"
 else
-	echo "go awayyyyyyy"
+	echo "go awayyyyyyy | va-t-en"
 fi
